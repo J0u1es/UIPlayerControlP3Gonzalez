924a9b195885eaf513e74482b24074ba6ef02243
Julia Gonzalez <145380988+J0u1es@users.noreply.github.com>
2024-02-29 11:41:48 -0800

    Worked on making the thing turn
    Added code to make the car move left and right.

--- U1PlayerControlGonzalez/Assets/Scripts/PlayerController.cs
+++ U1PlayerControlGonzalez/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
+    public float turnspeed;
+    public float horizontalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +16,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
         //Move the car forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnspeed * horizontalInput);
     }
 }
